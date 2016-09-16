d964bcb9f0626684d45d5b8404774e2ab50b0f32
LizRuelas <liz.ruelas.borda@gmail.com>
2016-09-16 12:18:02 -0700

    agregando validaciones

--- js/main.js
+++ js/main.js
@@ -1,43 +1,58 @@
 function validateForm(){
+	var spanName = document.createElement("span");
+	var referenciaName= document.getElementById("name");
+	var padreName=referenciaName.parentNode;
+	padreName.insertBefore(spanName,referenciaName);
+	var mensaje="";
+
 	var name = document.getElementById("name").value;
 	if (name == ""){
-		var span = document.createElement("span");
-		var referencia= document.getElementById("name");
-		var padre=referencia.parentNode;
-		padre.insertBefore(span,referencia);
-		var texto = document.createTextNode("Debe ingresar su nombre");
-		span.appendChild(texto);
-		return span;
+		mensaje="Debe ingresar su nombre"
+		
 	} else if (/^[a-z]/.test(name.charAt(0))){
-		alert("Por favor ingrese la primera letra en mayuscula");
+		
+		mensaje="Debe ingresar su nombre2"
+
 	} else if (/^[0-9]/.test(name.charAt(0))){
-		alert("Por favor no ingrese numeros");
+		mensaje="Debe ingresar su nombre3"
+		
+	} else padreName.removeChild(padreName.childNodes[2]);
+
+	if (mensaje.length>0){
+		var textoName = document.createTextNode(mensaje);
+		spanName.appendChild(textoName);
 	}
 
 	var lastname = document.getElementById("lastname").value;
 	if (lastname == ""){
-		alert("llenar el campo apellido por favor");
+		var spanApellido = document.createElement("span");
+		var referenciaApellido= document.getElementById("lastname");
+		var padreApellido=referenciaApellido.parentNode;
+		padreApellido.insertBefore(spanApellido,referenciaApellido);
+		var textoApellido = document.createTextNode("Debe ingresar su apellido");
+		spanApellido.appendChild(textoApellido);
+		//return spanApellido;
 	}
 
 	var correovalidado=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 	var email = document.getElementById("input-email").value;
 	if (email == "" ){
-		alert("llenar el campo correo por favor");
+		//alert("llenar el campo correo por favor");
 	} else if(!correovalidado.test(email)) {
-        alert("Ingrese un correo válido.");
+        //alert("Ingrese un correo válido.");
     }
 
 	var password = document.getElementById("input-password").value;
 	if (password == "") {
-		alert("llenar el campo contraseña por favor");
+		//alert("llenar el campo contraseña por favor");
 	} else if (password.length < 6){ 
-     	alert("Ingrese  al menos 6 caracteres"); 
+     	//alert("Ingrese  al menos 6 caracteres"); 
      } else if (password == "123456" || password == "password" || password == "098754"){
-    	alert("ingresa contraseña valida");
+    	//alert("ingresa contraseña valida");
     }
 
     var menu = document.querySelector("select").value;
 	if (menu == 0) {
-		alert("selecciona un tipo de bici por favor");
+		//alert("selecciona un tipo de bici por favor");
 	}  
 }
